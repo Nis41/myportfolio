07640363ee61f976aaf82322bc2eee00ff186798
PanchuSena <nhp0640@gmail.com>
2020-05-25 01:02:09 +0530

    Discuss Section done

--- src/Components/HomePage/Discuss/Discuss.jsx
+++ src/Components/HomePage/Discuss/Discuss.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import Bg1 from "./Images/1.jpg";
 import "./Discuss.css";
 
 const Discuss = () => {
   return (
     <div className="discussMainDiv">
-      <div className="discussBgDiv">
-        <img src={Bg1} alt="discussBackgroundImage" />
+      <div className="discussContentDiv">
+        <div className="discussText">
+          <h1>Want to Create Your Own Business Website ?</h1>
+          <h3> Let's Discuss !</h3>
+        </div>
+        <div className="discussBtn">
+          <a
+            href="https://wa.me/917069766147"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsappBtn hvr-bounce-to-right"
+          >
+            Discuss &nbsp;
+            <i class="fa fa-whatsapp" aria-hidden="true"></i>
+          </a>
+        </div>
       </div>
-      <div className="discussContentDiv"></div>
     </div>
   );
 };
