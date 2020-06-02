d8bc0199aadf5c90e2ce1729cf9ae355440e9b8c
PanchuSena <nhp0640@gmail.com>
2020-06-02 15:22:48 +0530

    not found page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Header from "./Components/Common/Header/Header";
 import Footer from "./Components/Common/Footer/Footer";
 import HomePage from "./Components/HomePage/HomePage";
 import AboutPage from "./Components/AboutPage/AboutPage";
 import SkillsPage from "./Components/SkillsPage/SkillsPage";
 import ContactPage from "./Components/ContactPage/ContactPage";
+import pageNotFound from "./Components/404Page/404Page";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -15,11 +16,14 @@ function App() {
       <ToastContainer />
       <Header />
       <div className="content">
-        <Route path="/about" component={AboutPage} />
-        <Route path="/skills" component={SkillsPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/" exact component={HomePage} />
-        {/* <Redirect to="/"></Redirect> */}
+        <Switch>
+          <Route path="/about" component={AboutPage} />
+          <Route path="/skills" component={SkillsPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/notfound" component={pageNotFound} />
+          <Route path="/" exact component={HomePage} />
+          <Redirect to="/notfound" />
+        </Switch>
       </div>
       <Footer />
     </div>
