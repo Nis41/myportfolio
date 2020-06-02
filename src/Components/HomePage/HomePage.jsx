f1093e74ee48e94d5308bfd2667796907fb7a5cd
PanchuSena <nhp0640@gmail.com>
2020-06-02 21:55:01 +0530

    total views added

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Main from "./Main/Main";
 import About from "./About/About";
 import Skills from "./Skills/Skills";
 import Testimonials from "./Testimonials/Testimonials";
 import Discuss from "./Discuss/Discuss";
+import axios from "axios";
 import "./HomePage.css";
 
-const HomePage = () => {
-  return (
-    <React.Fragment>
-      <Main />
-      <About />
-      <Skills />
-      <Testimonials />
-      <Discuss />
-    </React.Fragment>
-  );
-};
+class HomePage extends Component {
+  state = {};
+  //  CONTACT_API_URL = "http://localhost:5000/api/contact";
+  CONTACT_API_URL = "https://nisargpatel-portfolio.herokuapp.com/api/count";
+  componentDidMount() {
+    fetch(this.CONTACT_API_URL, {
+      method: "PUT",
+    })
+      .then(() => {
+        // console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Main />
+        <About />
+        <Skills />
+        <Testimonials />
+        <Discuss />
+      </React.Fragment>
+    );
+  }
+}
 
 export default HomePage;
